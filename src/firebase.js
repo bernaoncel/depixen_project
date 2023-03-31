094e7073cc6ef466a36b53f9d332af8497fc59a4
bernaoncel <61271962+bernaoncel@users.noreply.github.com>
2023-03-31 13:35:39 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDNS5C0bv9iMVIJT5yJgkuo2_LXd9ocJ5E",
-  authDomain: "depixen-11ed4.firebaseapp.com",
-  projectId: "depixen-11ed4",
-  storageBucket: "depixen-11ed4.appspot.com",
-  messagingSenderId: "371250510243",
-  appId: "1:371250510243:web:c4e1f6e8e683701ab1c216",
-  measurementId: "G-9S866XKP4W"
+  apiKey: "your_api_key",
+  authDomain: "your_auth_domain",
+  projectId: "your_project_id",
+  storageBucket: "your_storage_bucket",
+  messagingSenderId: "yor_messaging_sender_id",
+  appId: "your_app_id",
+  measurementId: "your_measurement_id"
 };
 
 // Initialize Firebase
